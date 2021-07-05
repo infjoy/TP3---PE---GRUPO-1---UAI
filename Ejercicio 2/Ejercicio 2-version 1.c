c4d03bf71b67cc8a8c5d0abe4632d95cdfa26d62
ericluna21 <facundoluna2001@gmail.com>
2021-07-04 22:25:35 -0300

    PDF1

--- Ejercicio 2/Ejercicio 2-version 1.c
+++ Ejercicio 2/Ejercicio 2-version 1.c
@@ -8,7 +8,7 @@
 
 int main(int argc, char *argv[]) {
 int i=0, nota=0, aplazados=0,numeroalumno=0;
-	for(i;i<=9;i++){
+	for(i;i<=20;i++){
         printf("Ingrese notas del alumno %d : ", i+0);
         scanf("%d",&nota);
         if (nota <5 && nota > 0 ){
